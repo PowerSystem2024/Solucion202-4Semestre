c30e7a8c81951d56e2ffb78416f140be873c8eed
Gimena <gimegauna12345678@gmail.com>
2025-09-15 00:26:04 -0300

    Corrijo: PERN-stack ahora es carpeta normal (no submódulo)

---
+++ JavaScript/Clase 05 Proyecto con stack pern por Daniel Guerrero - Parte 1/PERN-stack/src/router/tareas.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { 
+  actualizarTarea, 
+  crearTarea, 
+  eliminarTarea, 
+  listarTarea, 
+  listarTareas 
+} from "../controllers/tareas.controller.js";
+
+const router = Router();
+
+// Rutas de tareas
+router.get("/tareas", listarTareas);
+router.get("/tareas/:id", listarTarea);
+router.post("/tareas", crearTarea);
+router.put("/tareas/:id", actualizarTarea);
+router.delete("/tareas/:id", eliminarTarea);
+
+export default router;
